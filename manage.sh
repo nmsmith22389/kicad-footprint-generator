78ba278a6228c8001f1a447569108733cef62f6b
Jonas Otto (ottojo) <jottosmail@gmail.com>
2020-10-08 20:47:00 +0200

    revert unintentional changes to manage.sh

--- manage.sh
+++ manage.sh
@@ -23,7 +23,7 @@ update_dev_packages() {
 pep8_check() {
     echo ''
     echo '[!] Running pep8 check'
-    pycodestyle --max-line-length=120 "$KICADMODTREE_DIR/"
+    pep8 --max-line-length=120 "$KICADMODTREE_DIR/"
 }
 
 flake8_check() {
