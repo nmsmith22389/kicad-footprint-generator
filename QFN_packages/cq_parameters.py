b9583e81c239ccd12608ac0bbd58fb40592f21fd
Carlos Nieves <cnieves.mail@gmail.com>
2022-09-01 10:37:09 +0000

    Add Bourns TBU-CA family

--- QFN_packages/cq_parameters.py
+++ QFN_packages/cq_parameters.py
@@ -1960,6 +1960,33 @@ kicad_naming_params_qfn = {
         rotation = -90, # rotation if required
         dest_dir_prefix = '../Housings_DFN_QFN.3dshapes/'
         ),
+    'Fuse_Bourns_TBU-CA': Params( # 6.5x4, 3 pins, 0.9mm height
+        #Example - https://www.bourns.com/data/global/pdfs/TBU-CA.pdf
+        c = 0.2,        # pin thickness, body center part height
+#        K=0.2,          # Fillet radius for pin edges
+        L = 1.85,        # pin top flat part length (including fillet radius)
+        fp_s = True,     # True for circular pinmark, False for square pinmark (useful for diodes)
+        fp_r = 0.35,     # first pin indicator radius
+        fp_d = 0.1,     # first pin indicator distance from edge
+        fp_z = 0.02,     # first pin indicator depth
+        ef = 0.0, # 0.05,      # fillet of edges  Note: bigger bytes model with fillet
+        cce = 0.0,      #0.45 chamfer of the epad 1st pin corner
+        D = 6.5,       # body overall length
+        E = 4.0,       # body overall width
+        A1 = 0.025,  # body-board separation  maui to check
+        A2 = 0.9,  # body height
+        b = 3.4,  # pin width
+        e = 3.4,  # pin (center-to-center) distance
+        m = 0.3,  # margin between pins and body
+        ps = 'square',   # rounded pads
+        npx = 0,  # number of pins along X axis (width)
+        npy = 1,  # number of pins along y axis (length)
+        epad = (0.8,3.4), # e Pad #epad = None, # e Pad
+        excluded_pins = None, #no pin excluded
+        modelName = 'Fuse_Bourns_TBU-CA', #modelName
+        rotation = 0, # rotation if required
+        dest_dir_prefix = '../Fuse.3dshapes/'
+        ),
     'MLF-6-1EP_1.6x1.6mm_P0.5mm_EP0.5x1.26mm': Params( # from http://ww1.microchip.com/downloads/en/DeviceDoc/mic5353.pdf
         c = 0.15,        # pin thickness, body center part height
 #        K=0.2,          # Fillet radius for pin edges
