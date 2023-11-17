bc0e467e10d9e7082b088bbbef18dc2e77befd26
Carlos Nieves Ónega <cnieves.mail@gmail.com>
2023-11-17 09:48:16 +0100

    Remove debug printf

--- scripts/TerminalBlock_Wuerth/redcube.py
+++ scripts/TerminalBlock_Wuerth/redcube.py
@@ -4,7 +4,6 @@ import sys
 import os
 
 # load parent path of KicadModTree
-print ("Sys path0: "+sys.path[0])
 sys.path.append(os.path.join(sys.path[0], "..", "general"))
 
 import StandardBox_generator
