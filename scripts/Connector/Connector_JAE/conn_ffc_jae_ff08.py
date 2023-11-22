442e212bac2979c36b0a40a5f612ed135fd072fb
Carlos Nieves Ónega <cnieves.mail@gmail.com>
2023-11-22 13:44:07 +0100

    RoundToBase changed to roundG

--- scripts/Connector/Connector_JAE/conn_ffc_jae_ff08.py
+++ scripts/Connector/Connector_JAE/conn_ffc_jae_ff08.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.join(sys.path[0], "..", "..", ".."))  # load parent path
 from math import sqrt
 import argparse
 import yaml
-from helpers import *
+from drawing_tools import roundG
 from KicadModTree import *
 
 sys.path.append(os.path.join(sys.path[0], "..", "..", "tools"))  # load parent path of tools
@@ -162,11 +162,11 @@ def make_module(pin_count, configuration):
                                  width=configuration['fab_line_width'], layer='F.Fab'))
 
     ########################### CrtYd #################################
-    cx1 = roundToBase(bounding_box['left']-configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
-    cy1 = roundToBase(bounding_box['top']-configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
+    cx1 = roundG(bounding_box['left']-configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
+    cy1 = roundG(bounding_box['top']-configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
 
-    cx2 = roundToBase(bounding_box['right']+configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
-    cy2 = roundToBase(bounding_box['bottom']+configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
+    cx2 = roundG(bounding_box['right']+configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
+    cy2 = roundG(bounding_box['bottom']+configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
 
     kicad_mod.append(RectLine(
         start=[cx1, cy1], end=[cx2, cy2],
