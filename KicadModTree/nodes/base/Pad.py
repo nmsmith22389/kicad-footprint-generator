139d2703c05d2b7f971a6f63591edb0581eb65d1
John Beard <john.j.beard@gmail.com>
2024-10-19 15:32:26 +0800

    Simplify/fix ChamferSizeHandler a little
    This was (still is) a faily complete copy-paste of
    the RoundingRatioHandler above. However, just make some minor
    fixups for clarity:
    
    * Rework init clarity of getting 'chamfer_size' argument
    * If the chamfer is not square, raise, don't just throw away
      the other value, as that will be unexpected by the user.
    * Change 'rounding' to 'chamfer' from the incomplete copypaste job
    * Add tests on this class specifically. It's verbose and unclear
      to drive testing of this class through the Pad constructor.
    * Delete kicad4_compatible - this would have never worked because
      it didn't exist, so chamferRequested (was roundingRequested!)
      would crash when called.

--- KicadModTree/nodes/base/Pad.py
+++ KicadModTree/nodes/base/Pad.py
@@ -136,9 +136,9 @@ class ChamferSizeHandler(object):
     * *chamfer_ratio* (``float [0 <= r <= 0.5]``) --
       The chamfer ratio of the rounded rectangle. (default set by default_chamfer_ratio)
     * *maximum_chamfer* (``float``) --
-      The maximum radius for the rounded rectangle.
-      If the radius produced by the chamfer_ratio parameter for the pad would
-      exceed the maximum radius, the ratio is reduced to limit the radius.
+      The maximum chamfer size.
+      If the chamfer produced by the chamfer_ratio parameter for the pad would
+      exceed the maximum size, the ratio is reduced to limit the size.
       (This is useful for IPC-7351C compliance as it suggests 25% ratio with limit 0.25mm)
     * *chamfer_exact* (``float``) --
       Set an exact round chamfer size for a pad.
@@ -156,15 +156,23 @@ class ChamferSizeHandler(object):
 
         self.maximum_chamfer = getOptionalNumberTypeParam(
             kwargs, 'maximum_chamfer')
-        chamfer_size = toVectorUseCopyIfNumber(kwargs.get(
-            'chamfer_size'), low_limit=0, must_be_larger=False)
 
-        # ChamferedPad, ChamferedNativePad use chamfer_size vector
-        if (chamfer_size is not None) and ('chamfer_size' in kwargs.keys()) and (len(chamfer_size) > 1):
+        if kwargs.get('chamfer_size', None) is not None:
+            # Support the same vector or single number input as ChamferedPad
+            # does, but native pads can only have a chamfer_size vector that is the
+            # equal.
+            chamfer_size = toVectorUseCopyIfNumber(
+                kwargs["chamfer_size"], low_limit=0, must_be_larger=False
+            )
+
+            if chamfer_size.x != chamfer_size.y:
+                raise ValueError("chamfer_size must be a square vector for native pads")
+
             chamfer_size = chamfer_size[0]
         else:
             chamfer_size = None
 
+        # Override with chamfer_exact if it is set
         self.chamfer_exact = getOptionalNumberTypeParam(
             kwargs, 'chamfer_exact', default_value=chamfer_size)
 
@@ -172,19 +180,20 @@ class ChamferSizeHandler(object):
         r"""get the resulting chamfer ratio
 
         :param shortest_sidelength: shortest sidelength of a pad
-        :return: the resulting round radius ratio to be used for the pad
+        :return: the resulting chamfer ratio to be used for the pad
         """
 
         if self.chamfer_exact is not None:
             if self.chamfer_exact > shortest_sidelength/2:
                 raise ValueError(
-                    "requested round radius of {} is too large for pad size of {}"
+                    "requested chamfer of {} is too large for pad size of {}"
                     .format(self.chamfer_exact, shortest_sidelength)
                 )
             if self.maximum_chamfer is not None:
                 return min(self.chamfer_exact, self.maximum_chamfer)/shortest_sidelength
             else:
                 return self.chamfer_exact/shortest_sidelength
+
         if self.maximum_chamfer is not None:
             if self.chamfer_ratio*shortest_sidelength > self.maximum_chamfer:
                 return self.maximum_chamfer/shortest_sidelength
@@ -192,21 +201,18 @@ class ChamferSizeHandler(object):
         return self.chamfer_ratio
 
     def getChamferSize(self, shortest_sidelength):
-        r"""get the resulting round radius
+        r"""get the resulting chamfer size
 
         :param shortest_sidelength: shortest sidelength of a pad
-        :return: the resulting round radius to be used for the pad
+        :return: the resulting chamfer size to be used for the pad
         """
         return self.getChamferRatio(shortest_sidelength)*shortest_sidelength
 
-    def roundingRequested(self):
-        r"""Check if the pad has a rounded corner
+    def chamferRequested(self):
+        r"""Check if the handler indicates a non-zero chamfer
 
-        :return: True if rounded corners are required
+        :return: True if a chamfer is requested
         """
-        if self.kicad4_compatible:
-            return False
-
         if self.maximum_chamfer == 0:
             return False
 
@@ -223,8 +229,7 @@ class ChamferSizeHandler(object):
 
         :param limit: the new limit.
         """
-
-        if not self.roundingRequested():
+        if not self.chamferRequested():
             return
         if self.maximum_chamfer is not None:
             self.maximum_chamfer = min(self.maximum_chamfer, limit)
@@ -232,9 +237,9 @@ class ChamferSizeHandler(object):
             self.maximum_chamfer = limit
 
     def __str__(self):
-        return "ratio {}, max {}, exact {}, v4 compatible {}".format(
+        return "ratio {}, max {}, exact {}".format(
             self.chamfer_ratio, self.maximum_chamfer,
-            self.chamfer_exact, self.kicad4_compatible
+            self.chamfer_exact
         )
 
 
@@ -502,7 +507,7 @@ class Pad(Node):
 
     def _initChamferRatio(self, **kwargs):
 
-        if ('chamfer_size_handler' in kwargs) and (kwargs['chamfer_size_handler'] is not None):
+        if kwargs.get('chamfer_size_handler', None) is not None:
             self.chamfer_size_handler = kwargs['chamfer_size_handler']
         else:
             self.chamfer_size_handler = ChamferSizeHandler(**kwargs)
--- KicadModTree/tests/nodes/test_Pad.py
+++ KicadModTree/tests/nodes/test_Pad.py
@@ -1,5 +1,8 @@
+import pytest
+
 from KicadModTree import Vector2D
-from KicadModTree.nodes import Pad, Footprint, FootprintType
+from KicadModTree.nodes import Footprint, FootprintType
+from KicadModTree.nodes.base.Pad import Pad, ChamferSizeHandler
 from KicadModTree.util.corner_selection import CornerSelection
 
 from KicadModTree.tests.test_utils.fp_file_test import SerialisationTest
@@ -13,6 +16,19 @@ DEFAULT_FCU_KWARGS = {
     "shape": Pad.SHAPE_RECT,
     "type": Pad.TYPE_SMT,
     "layers": ["F.Cu"],
+}
+
+# EXample pad with a chamfer, but doesn't specify a size or ratio
+PAD_CHAMFER_KWARGS = {
+    "number": "42",
+    "at": Vector2D(0, 0),
+    "size": Vector2D(2, 2),  # not 1 to make sure size != ratio
+    "shape": Pad.SHAPE_ROUNDRECT,
+    "type": Pad.TYPE_SMT,
+    "layers": ["F.Cu"],
+    "chamfer_corners": CornerSelection(
+        corner_selection={CornerSelection.BOTTOM_LEFT: True}
+    ),
 }
 
 
@@ -66,24 +82,61 @@ class TestPadSerialisaion(SerialisationTest):
 
     def test_basic_chamfer_rounded(self):
 
-        corner = CornerSelection(corner_selection={CornerSelection.BOTTOM_LEFT: True})
-
-        # Copy the default args
-        pad_kwargs = DEFAULT_FCU_KWARGS.copy()
-
-        # Chamfer pads are always roundrect
-        pad_kwargs["shape"] = Pad.SHAPE_ROUNDRECT
+        # Copy the default args fo
+        pad_kwargs = PAD_CHAMFER_KWARGS.copy()
 
         # Leave the round radius handler and chamfer size handler as default
-
-        pad = Pad(chamfer_corners=corner, **pad_kwargs)
+        pad = Pad(**pad_kwargs)
 
         kicad_mod = Footprint("padtest", FootprintType.SMD)
         kicad_mod.append(pad)
 
+        # check the corner and handlers are set
+        assert pad.chamfer_size_handler.chamferRequested()
         assert pad.chamfer_ratio == 0.25
         assert pad.radius_ratio == 0.25
         assert pad.chamfer_corners.bottom_left
 
-        # check the corner and handlers are set
+        # And we can use this one to test the serialisation
         self.assert_serialises_as(kicad_mod, 'pad_chamfer_basic.kicad_mod')
+
+
+class TestChamferHandler:
+
+    def test_non_square(self):
+        # For now, the ChamferSizeHandler only supports square chamfers
+        # (and ChamferedPad does its own thing)
+        with pytest.raises(ValueError):
+            ChamferSizeHandler(
+                chamfer_size=Vector2D(0.1, 0.2)
+            )
+
+    def test_chamfer_size(self):
+
+        chamfer_handler = ChamferSizeHandler(
+            chamfer_size=Vector2D(0.1, 0.1)
+        )
+
+        assert chamfer_handler.chamferRequested()
+
+        # Computed values
+        assert chamfer_handler.getChamferRatio(2) == 0.05
+        assert chamfer_handler.getChamferSize(2) == 0.1
+
+    def test_chamfer_exact(self):
+
+        chamfer_handler = ChamferSizeHandler(
+            chamfer_exact=0.1
+        )
+
+        assert chamfer_handler.chamferRequested()
+
+        # Computed values
+
+        # Size on the limit of the maximums kicking in
+        assert chamfer_handler.getChamferRatio(0.2) == 0.1 / 0.2
+        assert chamfer_handler.getChamferSize(0.2) == 0.1
+
+        # Can't make a ratio larger than the size
+        with pytest.raises(ValueError):
+            chamfer_handler.getChamferRatio(0.1)
