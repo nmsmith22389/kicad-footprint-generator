d909170fcacfaa500a3fcaf86745730e80ef951d
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2024-11-26 21:43:20 +0100

    Support specifying multiple packages per run
    See merge request kicad/libraries/kicad-packages3D-generator!266

--- generator.py
+++ generator.py
@@ -37,7 +37,7 @@ def main():
     parser.add_argument(
         "-p",
         "--package",
-        nargs="*",
+        action="append",
         help="Selects a specific package configuration to generate models for.",
     )
     parser.add_argument("-v", "--verbose", help="Show OCCT output", action="store_true")
