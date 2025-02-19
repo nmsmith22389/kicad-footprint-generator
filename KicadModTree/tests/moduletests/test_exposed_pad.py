0368f0f54d11b16a157bcea1fdf0b5614adb5309
John Beard <john.j.beard@gmail.com>
2025-02-19 21:01:31 +0800

    Tests: fix discrepancy between file names and fooptrint names
    Causes kicad-cli to change the file on upgrade, which is confusing
    when validating changes.

--- KicadModTree/tests/moduletests/test_exposed_pad.py
+++ KicadModTree/tests/moduletests/test_exposed_pad.py
@@ -22,7 +22,7 @@ from KicadModTree.tests.test_utils.fp_file_test import SerialisationTest
 class TestExposedPads(SerialisationTest):
 
     def testSimpleExposedPad(self):
-        kicad_mod = Footprint("simple_exposed", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_simple", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, at=[0, 1], size=[2.1, 3],
@@ -32,7 +32,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, 'exposedpad_simple.kicad_mod')
 
     def testSimpleExposedPadNoRounding(self):
-        kicad_mod = Footprint("simple_no_rounding", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_no_rounding", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, at=[0, 1], size=[2.1, 3],
@@ -43,7 +43,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, 'exposedpad_no_rounding.kicad_mod')
 
     def testSimpleExposedMinimal(self):
-        kicad_mod = Footprint("simple_exposed_minimal", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_minimal", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[2.1, 3], paste_layout=2, via_layout=3
@@ -52,7 +52,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, 'exposedpad_minimal.kicad_mod')
 
     def testExposedPasteAutogenInner(self):
-        kicad_mod = Footprint("exposed_paste_autogen_inner", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_inner", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[5, 5], paste_layout=3, via_layout=2,
@@ -62,7 +62,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, 'exposedpad_autogen_inner.kicad_mod')
 
     def testExposedPasteAutogenInner2(self):
-        kicad_mod = Footprint("exposed_paste_autogen_inner2", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_inner2", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[5, 5], paste_layout=6, via_layout=4,
@@ -72,7 +72,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, 'exposedpad_autogen_inner2.kicad_mod')
 
     def testExposedPasteAutogenInnerAndOuter(self):
-        kicad_mod = Footprint("exposed_paste_autogen_autogen_inner_and_outer", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_inner_outer", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[12, 8], paste_between_vias=2,
@@ -84,7 +84,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, 'exposedpad_autogen_inner_outer.kicad_mod')
 
     def testExposedPasteAutogenInnerYonlyAndOuter(self):
-        kicad_mod = Footprint("exposed_paste_autogen_inner_y_only_and_outer", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_inner_y_and_outer", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[3, 5], paste_between_vias=2, paste_rings_outside=[2, 1], via_layout=[1, 3],
@@ -94,7 +94,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, "exposedpad_autogen_inner_y_and_outer.kicad_mod")
 
     def testExposedPasteAutogenInnerXonlyAndOuther(self):
-        kicad_mod = Footprint("exposed_paste_autogen_inner_x_only_and_outer", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_inner_x_and_outer", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[4, 3], paste_between_vias=2, paste_rings_outside=[1, 2], via_layout=[3, 1],
@@ -104,7 +104,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, "exposedpad_autogen_inner_x_and_outer.kicad_mod")
 
     def testExposedPasteAutogenOnlyOuter(self):
-        kicad_mod = Footprint("exposed_paste_autogen_only_outer", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_only_outer", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[2, 2], paste_between_vias=0, paste_rings_outside=[1, 1], via_layout=[1, 1],
@@ -114,7 +114,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, "exposedpad_autogen_only_outer.kicad_mod")
 
     def testExposedPasteBottomPadTests(self):
-        kicad_mod = Footprint("exposed_paste_autogen_bottom_pad", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_bottom_pad", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[2, 2], via_layout=[1, 1], at=[-2, -2],
@@ -130,7 +130,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, "exposedpad_autogen_bottom_pad.kicad_mod")
 
     def testExposed4x4paste(self):
-        kicad_mod = Footprint("exposed_paste_autogen_4x4", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_4x4", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=33, size=[3.55, 3.55],
@@ -150,7 +150,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, "exposedpad_autogen_4x4.kicad_mod")
 
     def testExposedPadEdgeCase1(self):
-        kicad_mod = Footprint("exposed_paste_autogen_case1", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_autogen_edgecase1", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[2, 2], via_layout=[2, 1], at=[-2, -2],
@@ -173,7 +173,7 @@ class TestExposedPads(SerialisationTest):
         self.assert_serialises_as(kicad_mod, "exposedpad_autogen_edgecase1.kicad_mod")
 
     def testExposedPasteViaTented(self):
-        kicad_mod = Footprint("exposed_paste_via_tented", FootprintType.SMD)
+        kicad_mod = Footprint("exposedpad_via_tented", FootprintType.SMD)
 
         kicad_mod.append(ExposedPad(
             number=3, size=[3, 3], via_layout=[2, 1], at=[-2, -2],
