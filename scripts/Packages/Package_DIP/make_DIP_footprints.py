b90eb551589e74f71d79e061b35dd8a50a15cac4
Kliment <kliment@0xfb.com>
2025-03-15 16:28:40 +0100

    Package_DIP: Fix output library name for some DIP parts

--- scripts/Packages/Package_DIP/make_DIP_footprints.py
+++ scripts/Packages/Package_DIP/make_DIP_footprints.py
@@ -119,14 +119,14 @@ if __name__ == '__main__':
     package_width=6.35
     for p in smd_pins:
         for prd in smd_pinrow_distances:
-            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, [], "Housings_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
+            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, [], "Package_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
     smd_pins=[4,6,8,10,12,14,16,18,20,22]
     pad_smd = [1.5, 1.78]
     smd_pinrow_distances=[9.53]
     package_width=6.35
     for p in smd_pins:
         for prd in smd_pinrow_distances:
-            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, ['Clearance8mm'], "Housings_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
+            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, ['Clearance8mm'], "Package_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
 
     smd_pins=[24,28,32,40,42,48,64]
     pad_smd = [2, 1.78]
@@ -134,14 +134,14 @@ if __name__ == '__main__':
     package_width=14.73
     for p in smd_pins:
         for prd in smd_pinrow_distances:
-            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, [], "Housings_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
+            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, [], "Package_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
     smd_pins=[40]
     pad_smd = [2, 1.78]
     smd_pinrow_distances=[25.24]
     package_width=24.89
     for p in smd_pins:
         for prd in smd_pinrow_distances:
-            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, [], "Housings_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
+            makeDIP(p, rm, prd, package_width, overlen_top, overlen_bottom, ddrill, pad_smd, True,  0,0,0, [], "Package_DIP", [0, 0, 0], [1, 1, 1], [0, 0, 0], 'SMDIP', 'surface-mounted (SMD) DIP', 'SMD DIP DIL PDIP SMDIP', outdir=output_dir)
 
     # Special DIP
     #
