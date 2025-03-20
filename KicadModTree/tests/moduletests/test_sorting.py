0dba090a42905995583d0b450ee9b7e026bb092b
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2025-03-20 11:18:29 +0100

    Add unit test for pad number sorting
    See merge request kicad/libraries/kicad-footprint-generator!1513

--- KicadModTree/tests/moduletests/test_sorting.py
+++ KicadModTree/tests/moduletests/test_sorting.py
@@ -1,5 +1,6 @@
-from KicadModTree import Footprint, Line, FootprintType
+from KicadModTree import Footprint, Line, FootprintType, Pad
 from kilibs.geom import Vector2D
+
 
 from KicadModTree.tests.test_utils.fp_file_test import SerialisationTest
 
@@ -19,3 +20,28 @@ class TestSmallValueSerialisation(SerialisationTest):
         kicad_mod.append(Line(start=Vector2D(-1e-15, 2), end=center))
 
         self.assert_serialises_as(kicad_mod, "test_sort_small_values.kicad_mod")
+
+    def testSortPadNumbers(self):
+        """
+        Test that pad numbers gets sorted correctly
+        """
+        kicad_mod = Footprint("test_sort_pad_numbers", FootprintType.SMD)
+
+        DEFAULT_PAD_KWARGS = {
+            "at": Vector2D(0, 0),
+            "size": Vector2D(1, 1),
+            "shape": Pad.SHAPE_RECT,
+            "type": Pad.TYPE_SMT,
+            "layers": ["F.Cu"],
+        }
+
+        center = Vector2D(0, 0)
+
+        kicad_mod.append(Pad(number="1", **DEFAULT_PAD_KWARGS))
+        kicad_mod.append(Pad(number="a1", **DEFAULT_PAD_KWARGS))
+        kicad_mod.append(Pad(number="a10", **DEFAULT_PAD_KWARGS))
+        kicad_mod.append(Pad(number="a2", **DEFAULT_PAD_KWARGS))
+        kicad_mod.append(Pad(number="", **DEFAULT_PAD_KWARGS))
+        kicad_mod.append(Pad(number="2", **DEFAULT_PAD_KWARGS))
+
+        self.assert_serialises_as(kicad_mod, "test_sort_pad_numbers.kicad_mod")
