1a50dcbe21c41c257237fef8029e22872ba40ba5
Uli Köhler <gitlab@techoverflow.net>
2025-05-30 16:22:52 +0200

    Add VH-compatible straight SMD connectors from Zhaoxing (YAML only)
    See merge request https://gitlab.com/kicad/libraries/kicad-footprint-generator/-/merge_requests/1252

--- scripts/Connector/Connector_SMD_single_row_plus_mounting_pad/generate.sh
+++ scripts/Connector/Connector_SMD_single_row_plus_mounting_pad/generate.sh
@@ -5,3 +5,4 @@ set -ex
 ./smd_single_row_plus_mounting_pad.py conn_hirose.yaml
 ./smd_single_row_plus_mounting_pad.py conn_jst.yaml
 ./smd_single_row_plus_mounting_pad.py conn_molex.yaml
+./smd_single_row_plus_mounting_pad.py conn_zhaoxing.yaml
